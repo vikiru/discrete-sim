97e4d183d5447b810bf06091a60d498936407b91
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-09 19:30:55 -0400

    Update alternate_simulator.py

--- SYSC4005_Group26_Project/alternate_simulator.py
+++ SYSC4005_Group26_Project/alternate_simulator.py
@@ -8,42 +8,40 @@ from simulator import *
 from workstation import *
 from inspector import *
 
-# save the results of each simulation run to a text file
-def alt_save_output_to_file(output_text, policy_num):
-    operating_policies = {
-        0: "original_policy",
-        1: "random_policy_less_probability_w1",
-        2: "random_policy_more_probability_w1",
-        3: "random_policy_equal_probability",
-        4: "reverse_priority",
-    }
+# define total # of replications
+number_of_replications = 10
 
-    # filename is where the results will be stored
-    file_name = "./alternate_results/" + operating_policies[policy_num] + ".txt"
-    f = open(file_name, "w")
-    f.write(output_text)
-    f.close()
-
+# define total # of replications and how long each replication simulates for
+sim_time = 50000
 
 if __name__ == "__main__":
-    # define total # of replications and how long each replication simulates for
-    sim_time = 50000
 
-    # in order to analyse the alternate policies,
+    # In order to analyse the alternate policies,
     # the alternate_simulator will run each policay for a simulation time of t = 50000
     # once and then repeat for the remaining policies
 
-    print("starting simulation for original policy, t = 50000...")
-    run_simulation(sim_time, 0)
+    print("Starting simulation for Original Policy, t = 50000...")
+    for i in range(1, number_of_replications + 1):
+        run_simulation(sim_time, 0, i)
+
     print(
-        "starting simulation for random policy, less probability for w1, t = 50000..."
+        "Starting simulation for Random Policy, Less Probability for W1, t = 50000..."
     )
-    run_simulation(sim_time, 1)
+    for i in range(1, number_of_replications + 1):
+        run_simulation(sim_time, 1, i)
+
     print(
-        "starting simulation for random policy, more probability for w1, t = 50000..."
+        "Starting simulation for Random Policy, More Probability for W1, t = 50000..."
     )
-    run_simulation(sim_time, 2)
-    print("starting simulation for random policy, equal probability, t = 50000...")
-    run_simulation(sim_time, 3)
-    print("starting simulation for reverse priority policy, t = 50000...")
-    run_simulation(sim_time, 4)
+    for i in range(1, number_of_replications + 1):
+        run_simulation(sim_time, 2, i)
+
+    print(
+        "Starting simulation for Random Policy, Equal Probability for W1, t = 50000..."
+    )
+    for i in range(1, number_of_replications + 1):
+        run_simulation(sim_time, 3, i)
+
+    print("Starting simulation for Reverse Priority Policy, t = 50000...")
+    for i in range(1, number_of_replications + 1):
+        run_simulation(sim_time, 4, i)
