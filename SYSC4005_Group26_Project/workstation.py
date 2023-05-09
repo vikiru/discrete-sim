d9f8abe3ba5e5c9beb5478b8df2aeb7cacc0c59d
vikiru <72267229+vikiru@users.noreply.github.com>
2023-05-09 18:48:33 -0400

    Update workstation.py

--- SYSC4005_Group26_Project/workstation.py
+++ SYSC4005_Group26_Project/workstation.py
@@ -7,98 +7,101 @@ from input_modeling import *
 
 
 class Workstation1:
-    # Initialize Workstation
     def __init__(self, env):
+        """Initialize Workstation1 with default values."""
         self.env = env
         self.filename = "ws1.dat"
         self.c1_buffer = simpy.Container(self.env, 2, 0)
         self.process_time = get_input_data_mean_time(self.filename)
         self.action = env.process(self.run())
         self.products_created = 0
-        self.process_time = {0: []}  # index 0 for w1
-        self.idle_time = {0: []}  # index 0 for W1
+        self.process_times = {0: []}  # index 0 for w1
+        self.idle_times = {0: []}  # index 0 for W1
 
-    # Used to identify the Workstation
     def get_name(self):
+        """Used to identify Workstation1."""
         return "W1"
 
-    # Main process loop,
-    # Workstation will try to get its required components, if it is able to get a component then
-    # it moves on to processing the component, updating both the idle and process time
-    # lists respectively
     def run(self):
+        """
+        Main process loop, Workstation1 will try to get its required components, if it
+        is able to get a component then it moves on to processing the component, updating
+        both the idle and process time lists respectively.
+        """
         while True:
             start_time = self.env.now
             yield self.c1_buffer.get(1)
             end_time = self.env.now - start_time
-            self.idle_time[0].append(end_time)
-            process_time = generate_random_mean_time(self.filename)
-            yield self.env.timeout(process_time)
-            self.process_time[0].append(process_time)
+            self.idle_times[0].append(end_time)
+            process_times = generate_random_mean_time(self.filename)
+            yield self.env.timeout(process_times)
+            self.process_times[0].append(process_times)
             self.products_created += 1
 
 
 class Workstation2:
-    # Initialize Workstation
     def __init__(self, env):
+        """Initialize Workstation2 with default values."""
         self.env = env
         self.filename = "ws2.dat"
         self.c1_buffer = simpy.Container(self.env, 2, 0)
         self.c2_buffer = simpy.Container(self.env, 2, 0)
-        self.process_time = get_input_data_mean_time(self.filename)
+        self.process_times = get_input_data_mean_time(self.filename)
         self.action = env.process(self.run())
         self.products_created = 0
-        self.process_time = {0: []}  # index 0 for w2
-        self.idle_time = {0: []}  # Index 0 for W2
+        self.process_times = {0: []}  # index 0 for w2
+        self.idle_times = {0: []}  # Index 0 for W2
 
-    # Used to identify the Workstation
     def get_name(self):
+        """Used to identify Workstation2."""
         return "W2"
 
-    # Main process loop,
-    # Workstation will try to get its required components, if it is able to get a component then
-    # it moves on to processing the component, updating both the idle and process time
-    # lists respectively
     def run(self):
+        """
+        Main process loop, Workstation2 will try to get its required components, if it
+        is able to get a component then it moves on to processing the component, updating
+        both the idle and process time lists respectively.
+        """
         while True:
             start_time = self.env.now
             yield self.c1_buffer.get(1) and self.c2_buffer.get(1)
             end_time = self.env.now - start_time
-            self.idle_time[0].append(end_time)
-            process_time = generate_random_mean_time(self.filename)
-            yield self.env.timeout(process_time)
-            self.process_time[0].append(process_time)
+            self.idle_times[0].append(end_time)
+            process_times = generate_random_mean_time(self.filename)
+            yield self.env.timeout(process_times)
+            self.process_times[0].append(process_times)
             self.products_created += 1
 
 
 class Workstation3:
-    # Initialize Workstation
     def __init__(self, env):
+        """Initialize Workstation3 with default values."""
         self.env = env
         self.filename = "ws3.dat"
         self.c1_buffer = simpy.Container(self.env, 2, 0)
         self.c3_buffer = simpy.Container(self.env, 2, 0)
-        self.process_time = get_input_data_mean_time(self.filename)
+        self.process_times = get_input_data_mean_time(self.filename)
         self.action = env.process(self.run())
         self.products_created = 0
-        self.process_time = {0: []}  # index 0 for w3
-        self.idle_time = {0: []}  # Index 0 for W3
+        self.process_times = {0: []}  # index 0 for w3
+        self.idle_times = {0: []}  # Index 0 for W3
 
-    # Used to identify the Workstation
     def get_name(self):
+        """Used to identify Workstation3."""
         return "W3"
 
-    # Main process loop,
-    # Workstation will try to get its required components, if it is able to get a component then
-    # it moves on to processing the component, updating both the idle and process time
-    # lists respectively
     def run(self):
+        """
+        Main process loop, Workstation3 will try to get its required components, if it
+        is able to get a component then it moves on to processing the component, updating
+        both the idle and process time lists respectively.
+        """
         while True:
             start_time = self.env.now
             yield self.c1_buffer.get(1) and self.c3_buffer.get(1)
             end_time = self.env.now - start_time
-            self.idle_time[0].append(end_time)
-            process_time = generate_random_mean_time(self.filename)
-            yield self.env.timeout(process_time)
-            self.process_time[0].append(process_time)
+            self.idle_times[0].append(end_time)
+            process_times = generate_random_mean_time(self.filename)
+            yield self.env.timeout(process_times)
+            self.process_times[0].append(process_times)
             self.products_created += 1
