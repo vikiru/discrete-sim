475a7eeccc73dd8c15c2d6e1c5f13b84f0e75c66
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-05-10 18:26:26 -0400

    Update alternate_simulator.py

--- SYSC4005_Group26_Project/alternate_simulator.py
+++ SYSC4005_Group26_Project/alternate_simulator.py
@@ -1,6 +1,6 @@
-# sysc4005 project - group 26
-# author: visakan kirubakaran
-# year of completion: 2022
+# SYSC4005 Project - Group 26
+# Author: Visakan Kirubakaran
+# Year of Completion: Jan - Apr 2022
 
 import simpy
 from scipy import stats
